3df7485022e4f6ad7ea26f4eb4cb9b982e5af39e
milind-okay <milind0359@gmail.com>
2016-01-25 17:49:02 +0530

    Initial commit

---
+++ app/src/main/java/in/magamestheory/twonone/MActivityFragment.java
@@ -0,0 +1,22 @@
+package in.magamestheory.twonone;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MActivityFragment extends Fragment {
+
+    public MActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_m, container, false);
+    }
+}
---
+++ app/src/main/java/in/magamestheory/twonone/OnlinePlayLog.java
@@ -0,0 +1,13 @@
+package in.magamestheory.twonone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnlinePlayLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_play_log);
+    }
+}
